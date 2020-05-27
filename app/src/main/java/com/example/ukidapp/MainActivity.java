59404440b43f32a67ff7a1b901417b627ed13d52
sion <parksion9262@gmail.com>
2020-05-27 23:08:09 +0900

    navigation menu background 변경 및 불 필요 menu 삭제 - 박시온

--- app/src/main/java/com/example/ukidapp/MainActivity.java
+++ app/src/main/java/com/example/ukidapp/MainActivity.java
@@ -3,16 +3,15 @@ package com.example.ukidapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Menu;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -81,14 +80,6 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-    ;
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
